867e4e7baa0a323612b5f08bb0e13fb5a8e29f3d
unknown <arunava82@gmail.com>
2014-08-19 19:39:29 +0530

    added next prv rand

--- xkcd.js
+++ xkcd.js
@@ -17,10 +17,13 @@ var server = app.listen(port ,ip_addr, function(){
 function onQuery(req, res , next){
 
 	var isRandom = req.param('random');
+	var _id = req.param('id');
 	var _path = "";
 	if(isRandom == "1"){
 		_path = Math.floor(Math.random() * 1409 );
 		_path = "" + _path + "/";
+	}else if(_id !=null && _id != ""){
+		_path = "" + _id + "/";
 	}
 	var options = {
 		host: 'xkcd.com',
@@ -39,6 +42,10 @@ function onQuery(req, res , next){
 		var foundTitle = false;
 		var foundImage = false;
 		var gotData = false;
+		var foundNext = false;
+		var foundPrev = false;
+		var next_id = "";
+		var previous_id = "";
 		var source ="";
 		var alt ="";
 		var title ="";
@@ -46,7 +53,6 @@ function onQuery(req, res , next){
 
 		resp.on('end', function () {
 
-			console.log(response);
 			var parser = new htmlparser.Parser({
 				onopentag: function(tagname, attribs){
 					if(tagname === "div" && attribs.id === 'ctitle'){
@@ -55,6 +61,27 @@ function onQuery(req, res , next){
 					if(tagname === "div" && attribs.id === 'comic'){
 						foundImage = true;
 					}
+					if(!foundPrev && tagname === "a" && attribs.rel === 'prev'){
+						var url = attribs.href;
+						if (url != '#'){
+							var parts = url.split('/')
+							previous_id = parts[parts.length-2];
+						}else{
+							previous_id = "0";
+						}
+						foundPrev = true;
+					}
+					if(!foundNext && tagname === "a" && attribs.rel === 'next'){
+						var url = attribs.href;
+						if (url != '#'){
+							var parts = url.split('/')
+							next_id = parts[parts.length-2];
+						}else{
+							next_id ="0";
+						}
+						foundNext = true;
+					}
+					
 					if(foundImage && tagname === "img"){
 						source = attribs.src;
 						alt = attribs.title;
@@ -73,7 +100,12 @@ function onQuery(req, res , next){
 				},
 				onclosetag: function(tagname){
 					if (gotData){
-						var resTxt = "<html><body><center style='font-size:30px;font-family:Arial'>" + title +"</center><br>";
+						var resTxt = "<html><body><center style='font-size:30px;font-family:Arial'>" ;
+						resTxt += "<a href='/xkcd?id=" + previous_id +"'>&lt;&lt;</a>" ;
+						resTxt += "&nbsp;&nbsp;<a href='/xkcd?random=1' style='font-size:20px;font-family:Arial'>Random</a>&nbsp;&nbsp;" ;
+						resTxt += "<a href='/xkcd?id=" + next_id +"'>&gt;&gt;</a>" ;
+						resTxt += "<br>" + title ;
+						resTxt += "</center><br>";
 						resTxt += "<img src='" + source + "'><br>";
 						resTxt += "<div style='font-size:5px'>" + alt + "</font>";
 						resTxt += "</body><html>";
